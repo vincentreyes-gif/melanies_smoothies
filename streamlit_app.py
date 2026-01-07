4ddac6a870ad10b771a9e1957319c32d212fa960
vincentreyes-gif <vincent.reyes@novare.com.ph>
2026-01-07 10:46:22 +0800

    Fix title formatting in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from snowflake.snowpark.functions import col
 
 
 # Write directly to the app
-st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:", use_container_width=True)
+st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
   """Choose the fruits you want in your custom Smoothie!"""
 )
